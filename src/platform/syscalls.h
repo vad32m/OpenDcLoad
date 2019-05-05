bd51366719553feae5d8a3ff967f82bd7e36105d
Vadym Mishchuk <superoriginalmailboxname@gmail.com>
2019-05-05 14:41:39 +0300

    project setup: update documentation for syscalls header file
    Signed-off-by: Vadym Mishchuk <superoriginalmailboxname@gmail.com>

--- src/platform/syscalls.h
+++ src/platform/syscalls.h
@@ -1,4 +1,11 @@
+/**
+ * @file
+ * Prototypes of low-level syscalls, required by newlib-nano
+ * see https://sourceware.org/newlib/libc.html#Stubs
+ */
+
 #include <sys/types.h>
+
 
 int _read(int file, char *ptr, int len);
 int _write(int file, char *buffer, unsigned int count);
@@ -7,15 +14,3 @@ int _fstat(int file, struct stat *st);
 int _close(int file);
 int _isatty(int file);
 caddr_t _sbrk(int incr);
-
-// void _exit(int i);
-// int _open(const char *name, int flags, int mode);
-// int _link(char *old, char *new);
-// int _unlink(char *name);
-// int _stat(char *file, struct stat *st);
-// int _execve(char *name, char **argv, char **env);
-// int _fork();
-// int _getpid();
-// int _kill(int pid, int sig);
-// int times(struct tm *buf);
-// int _wait(int *status);
