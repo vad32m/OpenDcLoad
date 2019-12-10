863ed18d55706f261b855999f3459e2e2b59cf97
Vadym Mishchuk <superoriginalmailboxname@gmail.com>
2019-12-10 20:36:13 +0200

    debug subsystem: add assertion for checking whether current expression is NULL
    Signed-off-by: Vadym Mishchuk <superoriginalmailboxname@gmail.com>

--- src/debug/assertions.h
+++ src/debug/assertions.h
@@ -18,11 +18,15 @@ _assert_die(const char* file, const char* line);
 
 #define ASSERT_NULL(NUMBER)  if ((NUMBER) != 0) {_assert_die(__FILE__, ASSERT_STR(__LINE__));}
 
+#define ASSERT_NON_NULL(PTR) if ((PTR) == 0) {_assert_die(__FILE__, ASSERT_STR(__LINE__));}
+
 #define ASSERT_TRUE(EXPRESSION) if (!(EXPRESSION)) {_assert_die(__FILE__, ASSERT_STR(__LINE__));}
 
 #else //DEBUG
 
 #define ASSERT_PTR_VALID(PTR) ((void)0)
+
+#define ASSERT_NON_NULL(PTR) ((void)0)
 
 #define ASSERT_NULL(NUMBER) ((void)0)
 
