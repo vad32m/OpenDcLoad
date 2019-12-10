0af9cbf8e9f17671d4ea1a32f0cee318c6d59956
Vadym Mishchuk <superoriginalmailboxname@gmail.com>
2019-12-10 20:30:07 +0200

    platform: add error codes with description
    Signed-off-by: Vadym Mishchuk <superoriginalmailboxname@gmail.com>

---
+++ src/error_codes.h
@@ -0,0 +1,23 @@
+/**
+ * @file Description of error codes
+ */
+
+#ifndef SRC_ERROR_CODES_H_
+#define SRC_ERROR_CODES_H_
+
+/**
+ * No error occured
+ */
+#define ERR_OK         (0)
+
+/**
+ * Allocation error
+ */
+#define ERR_NOMEM     (-1)
+
+/**
+ * Invalid argument
+ */
+#define ERR_INARG     (-2)
+
+#endif /* SRC_ERROR_CODES_H_ */
